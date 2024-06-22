8c3bed17630194ff9cdf4a3716dc8e3ac1079097
ItayElf <thh123321@gmail.com>
2024-06-22 22:00:05 +0300

    Feat: added autocomplete tags

--- lib/components/pieces/filter/piece_tags_filter.dart
+++ lib/components/pieces/filter/piece_tags_filter.dart
@@ -19,6 +19,13 @@ class PieceTagsFilter extends StatefulWidget {
 
 class _PieceTagsFilterState extends State<PieceTagsFilter> {
   final _tagController = DynamicTagController<DynamicTagData<Tag>>();
+  late List<DynamicTagData<Tag>> _tagOptions;
+
+  @override
+  void initState() {
+    _tagOptions = widget.tags.map((e) => DynamicTagData(e.tag, e)).toList();
+    super.initState();
+  }
 
   @override
   void dispose() {
@@ -28,48 +35,96 @@ class _PieceTagsFilterState extends State<PieceTagsFilter> {
 
   @override
   Widget build(BuildContext context) {
-    return TextFieldTags<DynamicTagData<Tag>>(
-      textfieldTagsController: _tagController,
-      textSeparators: const [' ', ','],
-      validator: (tag) {
-        if (_tagController.getTags!.any((element) => element.tag == tag.tag)) {
-          return "Already added";
-        }
-        return null;
+    return Autocomplete<DynamicTagData<Tag>>(
+      displayStringForOption: (e) => e.tag,
+      optionsBuilder: (value) {
+        if (value.text == "") return const [];
+        return _tagOptions.where((element) =>
+            element.tag.toLowerCase().contains(value.text.toLowerCase()));
       },
-      initialTags:
-          widget.notifier.value.map((e) => DynamicTagData(e.tag, e)).toList(),
-      inputFieldBuilder: (context, inputFieldValues) => TextField(
-        controller: inputFieldValues.textEditingController,
-        focusNode: inputFieldValues.focusNode,
-        decoration: InputDecoration(
-          isDense: true,
-          helperText: "Tags to filter",
-          errorText: inputFieldValues.error,
-          prefixIconConstraints: BoxConstraints(
-            maxWidth: MediaQuery.of(context).size.width * 0.74,
-          ),
-          prefixIcon: inputFieldValues.tags.isEmpty
-              ? null
-              : SingleChildScrollView(
-                  controller: inputFieldValues.tagScrollController,
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: inputFieldValues.tags
-                        .map((e) => getTagContainer(inputFieldValues, e))
-                        .toList(),
-                  ),
+      optionsViewBuilder: (context, onSelected, options) => ListView.builder(
+        itemCount: options.length,
+        itemBuilder: (context, index) => Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            width: MediaQuery.of(context).size.width / 3,
+            color: fromCssColor(options.elementAt(index).data.color).alpha == 0
+                ? Colors.white
+                : fromCssColor(options.elementAt(index).data.color),
+            child: TextButton(
+              onPressed: () => onSelected(options.elementAt(index)),
+              child: Text(
+                textAlign: TextAlign.left,
+                "#${options.elementAt(index).tag}",
+                style: const TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontSize: 16,
+                  color: Colors.black,
                 ),
+              ),
+            ),
+          ),
         ),
-        onSubmitted: (value) {
-          final tags = widget.tags.where((element) => element.tag == value);
-          if (tags.length == 1) {
-            inputFieldValues.onTagSubmitted(DynamicTagData(value, tags.first));
-            widget.notifier.value.add(tags.first);
-          }
-          inputFieldValues.focusNode.requestFocus();
-        },
       ),
+      fieldViewBuilder:
+          (context, textEditingController, focusNode, onFieldSubmitted) =>
+              TextFieldTags<DynamicTagData<Tag>>(
+        textfieldTagsController: _tagController,
+        textEditingController: textEditingController,
+        focusNode: focusNode,
+        textSeparators: const [' ', ','],
+        validator: (tag) {
+          if (_tagController.getTags!
+              .any((element) => element.tag == tag.tag)) {
+            return "Already added";
+          }
+          return null;
+        },
+        initialTags:
+            widget.notifier.value.map((e) => DynamicTagData(e.tag, e)).toList(),
+        inputFieldBuilder: (context, inputFieldValues) => TextField(
+          controller: inputFieldValues.textEditingController,
+          focusNode: inputFieldValues.focusNode,
+          decoration: InputDecoration(
+            isDense: true,
+            helperText: "Tags to filter",
+            errorText: inputFieldValues.error,
+            prefixIconConstraints: BoxConstraints(
+              maxWidth: MediaQuery.of(context).size.width * 0.74,
+            ),
+            prefixIcon: inputFieldValues.tags.isEmpty
+                ? null
+                : SingleChildScrollView(
+                    controller: inputFieldValues.tagScrollController,
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: inputFieldValues.tags
+                          .map((e) => getTagContainer(inputFieldValues, e))
+                          .toList(),
+                    ),
+                  ),
+          ),
+          onSubmitted: (value) {
+            final tags = widget.tags.where((element) => element.tag == value);
+            if (tags.length == 1) {
+              inputFieldValues
+                  .onTagSubmitted(DynamicTagData(value, tags.first));
+              widget.notifier.value.add(tags.first);
+            }
+            inputFieldValues.focusNode.requestFocus();
+          },
+        ),
+      ),
+      onSelected: (option) {
+        final tags = widget.tags.where((element) => element.tag == option.tag);
+        final isAdded =
+            _tagController.getTags?.where((e) => e.tag == option.tag).isEmpty ??
+                false;
+        if (tags.length == 1 && isAdded) {
+          _tagController.onTagSubmitted(DynamicTagData(option.tag, tags.first));
+          widget.notifier.value.add(tags.first);
+        }
+      },
     );
   }
 
