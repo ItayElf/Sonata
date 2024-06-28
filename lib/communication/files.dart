f9edb0608c9d67a7e2621c71181378b4cbd0c38c
ItayElf <thh123321@gmail.com>
2024-06-28 15:22:51 +0300

    Feat: added correct mime type

--- lib/communication/files.dart
+++ lib/communication/files.dart
@@ -1,13 +1,21 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:http_parser/http_parser.dart';
 import 'package:http/http.dart' as http;
+import 'package:mime/mime.dart';
 import 'package:sonata/communication/base.dart';
 import 'package:sonata/communication/result.dart';
 import 'package:sonata/models/piece.dart';
 
 const _filesUrl = "$apiUrl/files";
 const fileViewUrl = "$apiUrl/files/file";
+
+Future<String?> _getMimeType(File file) async {
+  final bytes = await file.readAsBytes();
+  final mimeType = lookupMimeType(file.path, headerBytes: bytes);
+  return mimeType;
+}
 
 Future<Result<Piece>> uploadLinkRequest(
   String pieceId,
@@ -37,9 +45,12 @@ Future<Result<Piece>> uploadFileRequest(
 ) async {
   final request =
       http.MultipartRequest("POST", Uri.parse("$_filesUrl/upload_file"));
-  request.files.add(await http.MultipartFile.fromPath('file', file.path));
+  final contentType = MediaType.parse(await _getMimeType(file) ?? "");
+  request.files.add(await http.MultipartFile.fromPath('file', file.path,
+      contentType: contentType));
   request.fields["id"] = pieceId;
   request.headers["Authorization"] = "Bearer $accessToken";
+
   final response = await http.Response.fromStream(await request.send());
 
   if (response.statusCode != 200) {
